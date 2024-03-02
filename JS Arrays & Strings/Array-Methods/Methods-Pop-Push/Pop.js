56fb650df4410eb99bfa4e20a1b6229a6a63493e
Stoqn <podlobg@gmail.com>
2024-03-02 14:38:40 +0200

    up

---
+++ JS Arrays & Strings/Array-Methods/Methods-Pop-Push/Pop.js
@@ -0,0 +1,16 @@
+let nums = [10, 20, 30, 40, 50, 60, 70];
+
+console.log(nums.length); // 7
+console.log(nums.pop()); // 70
+console.log(nums.length); // 6
+console.log(nums); // [ 10, 20, 30, 40, 50, 60 ]
+
+let cars = ['BWM', 'Audi', 'Toyota']
+
+let lastCar = cars.pop();
+
+console.log(lastCar);
+
+// Removes the last element from an array and returns
+// that element
+// ▪ This method changes the length of the array
---
+++ JS Arrays & Strings/Array-Methods/Methods-Pop-Push/Push.js
@@ -0,0 +1,11 @@
+
+let nums = [10, 20, 30, 40, 50, 60, 70];
+
+console.log(nums.length); // 7
+console.log(nums.push(80)); // 8 (nums.length)
+console.log(nums); // [ 10, 20, 30, 40, 50, 60, 70, 80 ]
+
+let cars = ['BWM', 'Audi', 'Toyota'];
+
+cars.push('Mercedess')
+console.log(cars);
