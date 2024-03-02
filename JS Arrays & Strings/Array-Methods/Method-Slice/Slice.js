e477a1ac5bf9d84efd542ac78d3946d2cdc252d0
Stoqn <podlobg@gmail.com>
2024-03-02 15:31:36 +0200

    up

--- JS Arrays & Strings/Array-Methods/Method-Slice/Slice.js
+++ JS Arrays & Strings/Array-Methods/Method-Slice/Slice.js
@@ -2,3 +2,9 @@
 // portion of an array into a new array object selected
 // from begin to end (end not included)
 // ▪ The original array will not be modified
+
+let fruits = ['Banana', 'Orange', 'Lemon', 'Apple'];
+let citrus = fruits.slice(1, 3);
+let fruitsCopy = fruits.slice();
+// fruits contains ['Banana', 'Orange', 'Lemon', 'Apple']
+// citrus contains ['Orange', 'Lemon']
