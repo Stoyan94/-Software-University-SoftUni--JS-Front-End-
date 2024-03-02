7c1742ad6c747f4e0a380da7531649e1aa7ff511
Stoqn <podlobg@gmail.com>
2024-03-02 14:20:40 +0200

    up

---
+++ JS Arrays & Strings/Array destructuring and Rest-operator/Array-destructing-and-Rest-opr.js
@@ -0,0 +1,8 @@
+let number = [1, 2, 3, 4, 5, 6]
+
+let [firstNum, secondNum, thirdNum, ...restNumbers] = number;
+
+console.log(firstNum);
+console.log(secondNum);
+console.log(thirdNum);
+console.log(restNumbers);
