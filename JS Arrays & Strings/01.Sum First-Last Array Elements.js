940de60d0d9fdb3b0452c1301c8f400586989011
Stoqn <podlobg@gmail.com>
2024-03-02 13:29:35 +0200

    up

--- JS Arrays & Strings/01.Sum First-Last Array Elements.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    
-    let firstEl = input[0];
-    let lastEl = input[input.length-1]
-
-    console.log(firstEl+lastEl);
-}
-
-solve([20, 30, 40]);
---
+++ JS Arrays & Strings/Arrays-of-Different-Types/Arrays-of-Different-Types.js
@@ -0,0 +1,11 @@
+// Array holding numbers
+let numbers = [10, 20, 30, 40, 50];
+
+
+//Array holding strings
+let weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+// Array holding mixed data (not a good practice)
+let mixedArr = [20, new Date(), 'hello', {x:5, y:8}];
+
+console.log(mixedArr);
