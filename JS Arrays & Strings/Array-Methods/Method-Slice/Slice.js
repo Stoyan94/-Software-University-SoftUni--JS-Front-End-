4247bfd2aac4a888896ac519e6443bfe2981b68a
Stoqn <podlobg@gmail.com>
2024-03-02 15:31:06 +0200

    up

---
+++ JS Arrays & Strings/Array-Methods/Method-Slice/Slice.js
@@ -0,0 +1,4 @@
+// The slice() method returns a shallow copy of a
+// portion of an array into a new array object selected
+// from begin to end (end not included)
+// ▪ The original array will not be modified
