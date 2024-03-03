2952809895e0b747b157ce5b60b45b57b658ddb6
Stoqn <podlobg@gmail.com>
2024-03-03 13:16:46 +0200

    up

---
+++ JS Arrays & Strings/Manipulating Strings/Replace/Replace.js
@@ -0,0 +1,9 @@
+//replace(search, replacement)
+
+let text = "Hello, john@softuni.bg, you have been using john@softuni.bg in your registration.";
+let replacedText = text.replace(".bg", ".com");
+
+console.log(replacedText);
+
+// Hello, john@softuni.com, you have been using
+// john@softuni.bg in your registration.
