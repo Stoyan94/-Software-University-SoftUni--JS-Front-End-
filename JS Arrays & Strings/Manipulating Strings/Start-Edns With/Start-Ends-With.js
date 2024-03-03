8f8b71de05262d89022bcab66145677252bb2a68
Stoqn <podlobg@gmail.com>
2024-03-03 14:21:52 +0200

    up

---
+++ JS Arrays & Strings/Manipulating Strings/Start-Edns With/Start-Ends-With.js
@@ -0,0 +1,14 @@
+// Use startsWith() to determine whether a string begins with
+// the characters of a specified substring
+
+let text = "My name is John";
+console.log(text.startsWith('My')); // Expected output: true
+
+// ▪ Use endsWith() to determine whether a string ends with the
+// characters of a specified substring
+
+let text2 = "My name is John";
+console.log(text2.endsWith('John')); // Expected output: true
+
+
+// both methods return boolean values
