df1f88c5821cea0c85405143b1ca82f36acb5272
Stoqn <podlobg@gmail.com>
2024-03-03 13:11:49 +0200

    up

---
+++ JS Arrays & Strings/Manipulating Strings/Searching-and-Extracting Substrings/Searching-and-Extracting Substrings.js
@@ -0,0 +1,19 @@
+// Searching for Substrings
+// indexOf(substr)
+
+let str = "I am JavaScript developer";
+console.log(str.indexOf("Java")); // Expected output: 5
+console.log(str.indexOf("java")); // Expected output: -1
+
+// lastIndexOf(substr)
+
+let str2 = "Intro to programming";
+let last = str2.lastIndexOf("o");
+console.log(last); // Expected output: 11
+
+
+//Extracting Substrings
+
+let str3 = "I am JavaScript developer";
+let sub = str3.substring(5, 10);
+console.log(sub); // Expected output: JavaS
