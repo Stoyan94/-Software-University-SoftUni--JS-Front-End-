bdd03972916a7a2dcde80cc8a8a1fb053c70fa09
Stoqn <podlobg@gmail.com>
2024-03-03 10:24:41 +0200

    up

---
+++ JS Arrays & Strings/Array-Methods/Method-Includes/Includes.js
@@ -0,0 +1,8 @@
+// Determines whether an array contains a certain
+// element, returning true or false as appropriate
+
+let arr = ['a', 'b', 'c'];
+
+if (arr.includes('a')) {
+    console.log('yes');
+}
