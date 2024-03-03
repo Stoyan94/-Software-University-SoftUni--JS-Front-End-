c1cc73c975add31f9d208440b81c8923a206f935
Stoqn <podlobg@gmail.com>
2024-03-03 14:04:15 +0200

    up

---
+++ JS Arrays & Strings/Manipulating Strings/Repeat/Repeat.js
@@ -0,0 +1,21 @@
+
+let n = 3;
+for(let i = 1; i <= n; i++) {
+console.log('*'.repeat(i));
+}
+
+// *
+// **
+// ***
+
+function solve(text, word) {
+
+    while (text.includes(word)) {
+
+     text = text.replace(word, '*'.repeat(word.length));
+     
+    }
+    console.log(text);    
+}
+
+solve('A small sentence with some words, small', 'small')
