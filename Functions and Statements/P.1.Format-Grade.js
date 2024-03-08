78106f48162c6e3d98969b87bf13d4e9642f769f
Stoqn <podlobg@gmail.com>
2024-03-08 17:37:56 +0200

    up

---
+++ Functions and Statements/P.1.Format-Grade.js
@@ -0,0 +1,18 @@
+function solve(gradeExam) {
+    const gradesToCompare = [3.00, 3.00, 3.50, 4.50, 5.5];
+
+    let validateGrade = ChekGradeFromExam(gradeExam, gradesToCompare);
+}
+
+ChekGradeFromExam(gradeExam, gradesToCompare) {
+
+    for (const grade of gradesToCompare) {
+        if (gradeExam === grade) {
+            
+        }
+    }
+
+}
+
+solve('3.33');
+
