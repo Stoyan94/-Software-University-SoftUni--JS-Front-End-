23fd69318b13c276a1132357b75ed495e407ea4a
Stoqn <podlobg@gmail.com>
2024-03-10 20:01:43 +0200

    up

---
+++ Functions and Statements/test.js
@@ -0,0 +1,18 @@
+function printDNA(length) {
+    let sequence = "ATCGTTAGGG";
+
+    for (let i = 0; i < length; i++) {
+        let currentSymbol = sequence[i % sequence.length];
+
+        if (i % 4 === 0) {
+            console.log(`**${currentSymbol}**`);
+        } else if (i % 4 === 1 || i % 4 === 3) {
+            console.log(`*${currentSymbol}--${sequence[(i + 2) % sequence.length]}*`);
+        } else {
+            console.log(`${currentSymbol}----${sequence[(i + 2) % sequence.length]}`);
+        }
+    }
+}
+
+// Example usage
+printDNA(4);
