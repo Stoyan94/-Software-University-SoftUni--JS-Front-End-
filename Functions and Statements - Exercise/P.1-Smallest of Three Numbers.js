a39ee30c3d54856da14057704c05c2d317ed7a17
Stoyan <podlobg@gmail.com>
2024-03-15 17:11:34 +0200

    up

---
+++ Functions and Statements - Exercise/P.1-Smallest of Three Numbers.js
@@ -0,0 +1,10 @@
+function findSmalletsNumber(num1, num2, num3) {
+    const smalletsNum = Math.min(num1, num2, num3);
+
+    console.log(smalletsNum);
+}
+
+findSmalletsNumber(2,
+    5,
+    3
+    )
