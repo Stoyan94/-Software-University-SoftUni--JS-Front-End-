8a52ada565fa2a54fdbd2b10247a58659cb51384
Stoyan <podlobg@gmail.com>
2024-03-17 14:12:13 +0200

    up

---
+++ Objects and Classes- Lab/Create-obj-properties-hits.js
@@ -0,0 +1,20 @@
+//Multiline object literal (over 2 properties)
+let personInfo = {
+    firstName: 'Stoyan',
+    lastName: 'Stoyanov',
+    age: '20',
+}
+console.log(personInfo);
+
+//Object litelar with shorthand syntax
+let firstName = 'Maniac';
+let lastName = 'Hell';
+let age = 999;
+
+let personInfo2 = {
+    firstName,
+    lastName,
+    age,
+}
+
+console.log(personInfo2);
