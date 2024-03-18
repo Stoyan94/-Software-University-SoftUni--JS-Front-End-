4eb42f0ef7a8d25abd2e73bf46e289f0473b501f
Stoyan <podlobg@gmail.com>
2024-03-18 14:22:31 +0200

    up

---
+++ Objects and Classes- Lab/Solving Problems/P.8-Cats.js
@@ -0,0 +1,19 @@
+function solve(input) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = Number(age);
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    input
+        .map(line => line.split(' '))
+        .map(([name, age]) => new Cat(name, age))
+        .forEach(cat => cat.meow())
+}
+
+solve(['Mellow 2', 'Tom 5'])
