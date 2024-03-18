592073714aeb38a7a99cb3ae775def998cb99cf8
Stoyan <podlobg@gmail.com>
2024-03-18 10:20:44 +0200

    up

---
+++ Objects and Classes- Lab/P-5-Phone-Book.js
@@ -0,0 +1,21 @@
+function phoneBookInput(params) {
+    let phoneBook = {};
+    
+    for (const currParam of params) {
+        const [name, phone] = currParam.split(' ');
+        phoneBook[name] = phone;
+    }
+
+    Object.keys(phoneBook)
+    .forEach(name => console.log(`${name} -> ${phoneBook[name]}`))
+
+    for (const name in phoneBook) {
+        console.log(`${name} -> ${phoneBook[name]}`);
+    }
+}
+
+phoneBookInput(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344']
+);
