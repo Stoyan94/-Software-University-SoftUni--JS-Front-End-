44e556768579c4ea2d5c6325facec781acdf2c86
Stoyan <podlobg@gmail.com>
2024-03-19 17:14:25 +0200

    up

--- Exercise Objects and Classes/P.1-Employees.js
+++ Exercise Objects and Classes/P.1-Employees.js
@@ -4,7 +4,18 @@ function solve(input) {
         .forEach(employee => console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNumber}`))
 }
 
-solve([
+function solve2(employeeNames) {
+    const employees = [];
+
+    for (const name of employeeNames) {
+        const currEmp = {name, personalNumber: name.length}
+        employees.push(currEmp)       
+    }
+    
+    employees.forEach(employee => console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNumber}`))
+}
+
+solve2([
     'Silas Butler',
     'Adnaan Buckley',
     'Juan Peterson',
