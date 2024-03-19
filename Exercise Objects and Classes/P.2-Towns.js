ed24aebb67af4e54cc9f0b275bf92531ed31e543
Stoyan <podlobg@gmail.com>
2024-03-19 19:07:21 +0200

    up

--- Exercise Objects and Classes/P.2-Towns.js
+++ Exercise Objects and Classes/P.2-Towns.js
@@ -10,6 +10,24 @@ function solve(townsInfo) {
             .forEach(town => console.log(town));
 }
 
+function solve2(input) {
+    const towns = [];
+
+    for (const row of input) {
+        const [townName, latitude, longitude] = row.split(' | ');
+
+        const town = {
+            town: townName,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2),
+        };
+
+        towns.push(town);
+    }
+
+    towns.forEach(town => console.log(town));
+}
+
 solve(['Sofia | 42.696552 | 23.32601',
 'Beijing | 39.913818 | 116.363625']
 )
