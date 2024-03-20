0ed9a1dfd90847f4c5bd65066eb84a2943c321e5
Stoyan <podlobg@gmail.com>
2024-03-20 19:19:36 +0200

    up

--- Exercise Objects and Classes/P.6-Words-tracker.js
+++ Exercise Objects and Classes/P.6-Words-tracker.js
@@ -19,6 +19,28 @@ function solve(input) {
               .forEach(([word, occurrences]) => console.log(`${word} - ${occurrences}`));
 }
 
+function solve(input) {
+    const words = {};
+    const searchWords = input.shift().split(' ');
+
+    for (const searchWord of searchWords) {
+        words[searchWord] = 0;
+    }
+    
+    for (const word of input) {
+        if (words.hasOwnProperty(word)) {
+            words[word] += 1;
+        }
+    }
+
+    const sortedWords = Object.entries(words)
+        .sort((a, b) => b[1] - a[1]);
+
+    for (const [word, occurrences] of sortedWords) {
+        console.log(`${word} - ${occurrences}`)
+    }
+}
+
 solve([
     'this sentence',
     'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of', 'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
