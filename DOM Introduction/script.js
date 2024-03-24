574bd8239d4aa0572b42309f04ec1ab86689377d
Stoyan <podlobg@gmail.com>
2024-03-24 16:43:09 +0200

    up

--- DOM Introduction/script.js
+++ DOM Introduction/script.js
@@ -28,3 +28,32 @@ const contentLiveNodeList = contentElement.childNodes;
 const contentLiveHtmlCollection = contentElement.children;
 console.log(contentLiveNodeList);
 console.log(contentLiveHtmlCollection);
+
+// Iterate collections
+console.log('Iterate collections');
+
+console.log('HTML Collection');
+for (const htmlCollection of contentLiveHtmlCollection) {
+    console.log(htmlCollection);
+}
+
+console.log('Node Elements');
+for (const nodeElement of contentLiveNodeList) {
+    console.log(nodeElement);   
+}
+
+// for node list we can use and forEach
+console.log('ForEach nodes');
+contentLiveNodeList.forEach(nodeElement => console.log(nodeElement))
+
+// Convert to array
+console.log('Array version');
+console.log('HTML Collection array');
+// let htmlElementsArray = Array.from(contentLiveHtmlCollection)
+let htmlElementsArray = [...contentLiveHtmlCollection]
+console.log(htmlElementsArray);
+
+console.log('Nodes array');
+// let htmlNodessArray = Array.from(contentLiveNodeList)
+let htmlNodessArray = [...contentLiveNodeList]
+console.log(htmlNodessArray);
