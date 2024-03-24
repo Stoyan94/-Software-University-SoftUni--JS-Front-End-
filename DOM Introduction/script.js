cd26134adea94b8a6fb81e59d0c31e612b864425
Stoyan <podlobg@gmail.com>
2024-03-24 16:03:12 +0200

    up

--- DOM Introduction/script.js
+++ DOM Introduction/script.js
@@ -1,1 +1,20 @@
 console.log('Hello from an external javascript');
+
+const textInputElement = document.getElementById('uName')
+console.log(textInputElement);
+
+
+// setTimeout(() => {
+//     textInputElement.value = 'neshto'
+// }, 2000);
+
+const fancyInputElements = document.getElementsByClassName('fancy-input')
+console.log(fancyInputElements);
+
+// Get first input type text
+const firstInputTextElement = document.querySelector('.fancy-input[type=text]')
+console.log(firstInputTextElement);
+
+// Get all input type text elements
+const inputTextElements = document.querySelectorAll('.fancy-input[type=text]')
+console.log(inputTextElements);
