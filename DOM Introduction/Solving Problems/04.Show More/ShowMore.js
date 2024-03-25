843a50e137e3a2dae4860656d90d92b230403fe7
Stoyan <podlobg@gmail.com>
2024-03-25 14:11:44 +0200

    up

---
+++ DOM Introduction/Solving Problems/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const moreButton = document.getElementById('more');
+    const hiddenText = document.getElementById('text');
+
+    moreButton.style.display = 'none'
+    hiddenText.style.display = 'inline'
+}
