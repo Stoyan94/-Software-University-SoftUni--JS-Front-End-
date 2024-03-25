a9c404b4830fb88d5e8718abdc3cafbbef071120
Stoyan <podlobg@gmail.com>
2024-03-25 14:52:36 +0200

    up

--- DOM Introduction/Solving Problems/05.Colorize Table/ColorizeTable.js
+++ DOM Introduction/Solving Problems/05.Colorize Table/ColorizeTable.js
@@ -1,15 +1,19 @@
 function colorize() {
-    const rows = document.getElementsByTagName('tr');
+    const evenRowElemets =document.querySelectorAll('table tr:nth-child(2n)')
 
-    for (let i = 1; i < rows.length ; i++) {
-        if (i % 2 === 0) {
-            rows[i - 1].style.background = 'teal'
-        }
+    for (const evenRow of evenRowElemets) {
+        evenRow.style.backgroundColor = 'teal'
     }
+    
+    evenRowElemets.forEach(elemet => elemet.style.backgroundColor = 'teal')
 }
 
 // function colorize() {
-//     const evenRowElemets =document.querySelectorAll('table tr:nth-child(2n)')
-   
-//     evenRowElemets.forEach(elemet => elemet.style.backgroundColor = 'teal')
+//     const rows = document.getElementsByTagName('tr');
+
+//     for (let i = 1; i < rows.length ; i++) {
+//         if (i % 2 === 0) {
+//             rows[i - 1].style.background = 'teal'
+//         }
+//     }
 // }
