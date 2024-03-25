4aff160364174eccab54ecdd75fe688207383883
Stoyan <podlobg@gmail.com>
2024-03-25 13:59:33 +0200

    up

--- DOM Introduction/Solving Problems/03.Sum Numbers/SumNumbers.js
+++ DOM Introduction/Solving Problems/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const firstInputElement = document.getElementById('num1')
+    const seckondInputElement = document.getElementById('num2')
+    const sumInputElemet = document.querySelector('#sum')
+
+    const firstNumber = Number(firstInputElement.value);
+    const seckondNumber = Number(seckondInputElement.value)
+
+    sumInputElemet.value = firstNumber + seckondNumber;
 }
