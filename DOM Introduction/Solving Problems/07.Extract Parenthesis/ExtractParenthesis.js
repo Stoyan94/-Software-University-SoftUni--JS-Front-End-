1fa75683ee9f94574faeb24a664628956e1a4881
Stoyan <podlobg@gmail.com>
2024-03-25 15:02:56 +0200

    up

--- DOM Introduction/Solving Problems/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/Solving Problems/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+    contentElement = document.getElementById(content)
 
+    const matches = contentElement.textContent.matchAll(/\(([a-zA-Z ]+)\)/g)
+
+    const text = Array
+          .from(matches)
+          .map(match => match[1])
+          .join('; ')
+
+    return text;
 }
