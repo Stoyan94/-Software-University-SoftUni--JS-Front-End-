bd144b76dc5c5e761e11d08a46d0faa2d6093852
Stoyan <podlobg@gmail.com>
2024-03-26 15:45:43 +0200

    up

--- Exercise DOM Introduction/Demo live collections/script.js
+++ Exercise DOM Introduction/Demo live collections/script.js
@@ -1,6 +1,26 @@
+const movieListElement = document.getElementById('movies');
+
+const liveElementCollection = movieListElement.children;
+const liveNodeList = movieListElement.childNodes;
+
+const staticNodeList = document.querySelectorAll('#movies li');
+
+console.log(liveElementCollection);
+console.log(liveNodeList);
+console.log(staticNodeList);
+console.log('*************');
+setTimeout(() => {
+    const movieLiItem = document.createElement('li');
+    movieLiItem.textContent = 'Ghost';
+    movieListElement.appendChild(movieLiItem);
+    
+    console.log(liveElementCollection);
+    console.log(liveNodeList);
+    console.log(staticNodeList);
+}, 3000);
 
 
-
+// Node List can be static and live...depends on method we use
 
 // HTMLCollections are live collections that automatically update when the underlying document structure changes. Here are methods that return HTMLCollections:
 
