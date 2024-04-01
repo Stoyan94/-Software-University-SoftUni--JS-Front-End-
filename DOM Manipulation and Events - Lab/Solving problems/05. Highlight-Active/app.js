186dcd47f650c545b165c2f67234bd90c39b984c
Stoyan <podlobg@gmail.com>
2024-04-01 23:16:09 +0300

    up

---
+++ DOM Manipulation and Events - Lab/Solving problems/05. Highlight-Active/app.js
@@ -0,0 +1,11 @@
+function focused() {
+    const inputElements = document.querySelectorAll('input[type=text]');
+
+    Array.from(inputElements).forEach(inputElement => inputElement.addEventListener('focus', (event) => {
+        event.target.parentElement.classList.add('focused');
+    }));
+
+    Array.from(inputElements).forEach(inputElement => inputElement.addEventListener('blur', (event) => {
+        event.target.parentElement.classList.remove('focused');
+    }));
+}
