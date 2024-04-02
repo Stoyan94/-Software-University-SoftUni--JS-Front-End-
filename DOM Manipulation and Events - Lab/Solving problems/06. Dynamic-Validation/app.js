bcd3f785d2c99364d6756f6eb6ac83001681bac0
Stoyan <podlobg@gmail.com>
2024-04-02 19:25:37 +0300

    up

--- DOM Manipulation and Events - Lab/Solving problems/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events - Lab/Solving problems/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    const emailInputElement = document.getElementById('email')
+    const patenr = /^[a-z]+@[a-z]+\.[a-z]+/
+
+    emailInputElement.addEventListener('change', (event) => {
+        if (!patenr.test(event.target.value)) {
+            event.target.classList.add('error')
+        } else{
+            event.target.classList.remove('error')
+        }
+    })
 }
