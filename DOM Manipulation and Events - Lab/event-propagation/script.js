492201d286d8232fe11fc227b5d2c5f88d20ec2d
Stoyan <podlobg@gmail.com>
2024-04-02 18:32:50 +0300

    up

--- DOM Manipulation and Events - Lab/event-propagation/script.js
+++ DOM Manipulation and Events - Lab/event-propagation/script.js
@@ -4,12 +4,14 @@ const yellowElement = document.querySelector('.yellow')
 
 orangeElement.addEventListener('click', () => {
     console.log('Orange clicked');
-})
+}, {capture: true})
 
 greenElement.addEventListener('click', () => {
     console.log('Green clicked');
-})
+    
+}, {capture: false})
 
-yellowElement.addEventListener('click', () => {
+yellowElement.addEventListener('click', (event) => {
+    event.stopPropagation();
     console.log('Yellow clicked');
 })
