68fb4f3565f1c3bc0a28196a9085416a85f4b9fa
Stoyan <podlobg@gmail.com>
2024-04-03 17:46:19 +0300

    up

--- DOM Manipulation and Events - Lab/Solving problems/07. Shopping-Cart/solution.js
+++ DOM Manipulation and Events - Lab/Solving problems/07. Shopping-Cart/solution.js
@@ -6,26 +6,26 @@ function solve() {
    let uniqueProducts = {};
 
    for (const buttonElement of addProductButtonElements) {
-      const productElement  = buttonElement.parentElement.parentElement; //only for judge 
+      const productElement = buttonElement.parentElement.parentElement; //only for judge 
       // const product = buttonElement.closest('.product') this is the better option, but 'closest' doesn't work in judge
 
       buttonElement.addEventListener('click', () => {
-         const title = productElement .querySelector('.product-title').textContent
+         const title = productElement.querySelector('.product-title').textContent
          const price = Number(productElement.querySelector('.product-line-price').textContent)
         
          totalPrice += price;
          uniqueProducts[title] = true;
 
-         textareaElement.textContent += `Added ${title} for ${price.toFixed(2)} to the cart\n`
-      })
+         textareaElement.textContent += `Added ${title} for ${price.toFixed(2)} to the cart.\n`
+      });
 
    }
 
-   checkoutButtonElement.addEventListener('click', () => {
+   checkoutButtonElement.addEventListener('click', (event) => {
       Array.from(addProductButtonElements).forEach(buttonElement => buttonElement.setAttribute('disabled', 'disabled'))
-
+      event.target.setAttribute('disabled', 'disabled')
       const list = Object.keys(uniqueProducts).join(', ')
 
-      textareaElement.textContent += `You bought ${list} for ${totalPrice}\n.` 
+      textareaElement.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.\n` 
    })
 }
