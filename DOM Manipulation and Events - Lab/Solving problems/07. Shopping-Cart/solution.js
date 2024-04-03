58a9c6959387f442b7f308fdca151353718471ac
Stoyan <podlobg@gmail.com>
2024-04-03 17:10:50 +0300

    up

--- DOM Manipulation and Events - Lab/Solving problems/07. Shopping-Cart/solution.js
+++ DOM Manipulation and Events - Lab/Solving problems/07. Shopping-Cart/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-   //TODO...
+   const addProductButtonElements = document.querySelectorAll('button.add-product')
+   const textareaElement = document.querySelector('textarea')
+
+   for (const buttonElement of addProductButtonElements) {
+      const productElement  = buttonElement.parentElement.parentElement; //only for judge 
+      // const product = buttonElement.closest('.product') this is the better option, but 'closest' doesn't work in judge
+
+      buttonElement.addEventListener('click', () => {
+         const title = productElement .querySelector('product-title').textContent
+         const price = Number(productElement.querySelector('product-line-price').textContent)
+
+
+      })
+
+   }
 }
