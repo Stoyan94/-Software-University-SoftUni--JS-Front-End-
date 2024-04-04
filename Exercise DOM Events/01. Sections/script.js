639f39472b7074c3c0b648818333def14749953d
Stoyan <podlobg@gmail.com>
2024-04-04 17:28:25 +0300

    up

--- Exercise DOM Events/01. Sections/script.js
+++ Exercise DOM Events/01. Sections/script.js
@@ -1,3 +1,32 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentElement = document.getElementById('content')
+
+   const divElements = words
+      .map(word => {
+         const pElement = document.createElement('p');
+         pElement.textContent = word;
+         pElement.style.display = 'none';
+         
+         const divElement = document.createElement('div');
+         divElement.appendChild(pElement);
+
+         // divElement.addEventListener('click', () => {
+         //    pElement.style.display = 'block'
+         // })
+
+         return divElement;
+      });
+
+
+    divElements
+       .forEach(divElement => {
+          divElement.addEventListener('click', () => {
+             const pElement = divElement.querySelector('p').style.display = 'block';
+       })
+    });
+
+    // Better solution, but append doesn't work in Judge
+   //  contentElement.append(...divElements)   
+
+   divElements.forEach(divElement => contentElement.appendChild(divElement))
 }
