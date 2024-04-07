167aab086e12faab720fbd6162ec2f1c1020557e
Stoyan <podlobg@gmail.com>
2024-04-07 18:40:26 +0300

    up

--- Exercise DOM Events/06. Furniture/solution.js
+++ Exercise DOM Events/06. Furniture/solution.js
@@ -1,6 +1,6 @@
 function solve() {
-  const textAreaInputElement = document.querySelector('#exercise textarea:first-of-type');
-  const textAreaOutputElement = document.querySelector('#exercise textarea:last-of-type');
+  const textareaInputElement = document.querySelector('#exercise textarea:first-of-type');
+  const textareaOutputElement = document.querySelector('#exercise textarea:last-of-type');
 
   const generateButtonElement = document.querySelector('#exercise button:first-of-type');
   const buyButtonElement = document.querySelector('#exercise button:last-of-type');
@@ -8,39 +8,75 @@ function solve() {
   const furnitureTbodyElement = document.querySelector('.table tbody');
 
   generateButtonElement.addEventListener('click', (e) => {
-    const furnitures = JSON.parse(textAreaInputElement.value);
-    
-    for (const furniture of furnitures) {
-      const imageElement = document.createElement('img');
-      imageElement.src = furniture.img;
+      const furnitures = JSON.parse(textareaInputElement.value);
 
-      const imageTdElement = document.createElement('td');
-      imageElement.appendChild(imageElement);
+      for (const furniture of furnitures) {
+          const imgElement = document.createElement('img');
+          imgElement.src = furniture.img;
+          const imageTdElement = document.createElement('td');
+          imageTdElement.appendChild(imgElement);
 
-      const pNameElement = document.createElement('p');
-      pNameElement.textContent = furniture.name;
+          const namePElement = document.createElement('p');
+          namePElement.textContent = furniture.name;
+          const nameTdElement = document.createElement('td');
+          nameTdElement.appendChild(namePElement);
 
-      const tdNameElemant = document.createElement('td');
-      tdNameElemant.appendChild(pNameElement);
+          const pricePElement = document.createElement('p');
+          pricePElement.textContent = furniture.price;
+          const priceTdElement = document.createElement('td');
+          priceTdElement.appendChild(pricePElement);
 
-      const pPriceElement = document.createElement('p');
-      pPriceElement.textContent = furniture.price;
+          const decPElement = document.createElement('p');
+          decPElement.textContent = furniture.decFactor;
+          const decTdElement = document.createElement('td');
+          decTdElement.appendChild(decPElement);
 
-      const tdPriceElemant = document.createElement('td');
-      tdPriceElemant.appendChild(pPriceElement);
+          const markElement = document.createElement('input');
+          markElement.setAttribute('type', 'checkbox');
+          const markTdElement = document.createElement('td');
+          markTdElement.appendChild(markElement);
 
-      const pDecorElement = document.createElement('p');
-      pDecorElement.textContent = furniture.decFactor;
+          const furnitureTrElement = document.createElement('tr');
+          furnitureTrElement.appendChild(imageTdElement);
+          furnitureTrElement.appendChild(nameTdElement);
+          furnitureTrElement.appendChild(priceTdElement);
+          furnitureTrElement.appendChild(decTdElement);
+          furnitureTrElement.appendChild(markTdElement);
 
-      const tdDecorElemant = document.createElement('td');
-      tdDecorElemant.appendChild(pDecorElement);
+          furnitureTbodyElement.appendChild(furnitureTrElement);
+      }
+  });
 
-      const checkBoxElement = document.createElement('input');
-      checkBoxElement.setAttribute('type', 'checkbox')
+  buyButtonElement.addEventListener('click', (e) => {
+      let totalPrice = 0;
+      let totalDecorationFactor = 0;
+      let markChildren = 0;
+      let names = [];
 
-    }
+      Array.from(furnitureTbodyElement.children)
+           .forEach(furnitureTrElement => {
+              const markInputElement = furnitureTrElement.querySelector('input[type=checkbox]')
 
-  })
+              if (!markInputElement.checked) {
+                  return;
+              }
 
-   
+              const name = furnitureTrElement.children.item(1).textContent;
+              const price = Number(furnitureTrElement.children.item(2).textContent);
+              const decorationFactor = Number(furnitureTrElement.children.item(3).textContent);
+
+              names.push(name);
+              totalPrice += price;
+              totalDecorationFactor += decorationFactor;
+              markChildren++;
+           });
+
+            const averageDecorationFactor = totalDecorationFactor / markChildren;
+           textareaOutputElement.textContent += `Bought furniture: ${names.join(', ')}\n`;
+           textareaOutputElement.textContent += `Total price: ${totalPrice.toFixed(2)}\n`;
+           textareaOutputElement.textContent += `Average decoration factor: ${averageDecorationFactor}`;
+
+  });
+ 
 }
+
