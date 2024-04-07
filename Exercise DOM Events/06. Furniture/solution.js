d8af0ffb3dc6d422a3d70201d9d232972b0de7ff
Stoyan <podlobg@gmail.com>
2024-04-07 16:27:33 +0300

    up

--- Exercise DOM Events/06. Furniture/solution.js
+++ Exercise DOM Events/06. Furniture/solution.js
@@ -1,4 +1,46 @@
 function solve() {
+  const textAreaInputElement = document.querySelector('#exercise textarea:first-of-type');
+  const textAreaOutputElement = document.querySelector('#exercise textarea:last-of-type');
 
-  //TODO...
+  const generateButtonElement = document.querySelector('#exercise button:first-of-type');
+  const buyButtonElement = document.querySelector('#exercise button:last-of-type');
+
+  const furnitureTbodyElement = document.querySelector('.table tbody');
+
+  generateButtonElement.addEventListener('click', (e) => {
+    const furnitures = JSON.parse(textAreaInputElement.value);
+    
+    for (const furniture of furnitures) {
+      const imageElement = document.createElement('img');
+      imageElement.src = furniture.img;
+
+      const imageTdElement = document.createElement('td');
+      imageElement.appendChild(imageElement);
+
+      const pNameElement = document.createElement('p');
+      pNameElement.textContent = furniture.name;
+
+      const tdNameElemant = document.createElement('td');
+      tdNameElemant.appendChild(pNameElement);
+
+      const pPriceElement = document.createElement('p');
+      pPriceElement.textContent = furniture.price;
+
+      const tdPriceElemant = document.createElement('td');
+      tdPriceElemant.appendChild(pPriceElement);
+
+      const pDecorElement = document.createElement('p');
+      pDecorElement.textContent = furniture.decFactor;
+
+      const tdDecorElemant = document.createElement('td');
+      tdDecorElemant.appendChild(pDecorElement);
+
+      const checkBoxElement = document.createElement('input');
+      checkBoxElement.setAttribute('type', 'checkbox')
+
+    }
+
+  })
+
+   
 }
