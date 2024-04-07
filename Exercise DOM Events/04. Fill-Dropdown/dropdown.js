00395d9200e668014d95780e884d8df9f4b90d58
Stoyan <podlobg@gmail.com>
2024-04-07 15:35:08 +0300

    up

--- Exercise DOM Events/04. Fill-Dropdown/dropdown.js
+++ Exercise DOM Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+   const inputTextElement = document.getElementById('newItemText');
+   const inputValueElement = document.getElementById('newItemValue');
+   const selectElement = document.getElementById('menu');
+
+   const optionElement = document.createElement('option');
+   optionElement.value = inputValueElement.value;
+   optionElement.textContent = inputTextElement.value;
+
+   selectElement.appendChild(optionElement);
+
+   inputTextElement.value = ''; 
+   inputValueElement.value = '';
+
 }
