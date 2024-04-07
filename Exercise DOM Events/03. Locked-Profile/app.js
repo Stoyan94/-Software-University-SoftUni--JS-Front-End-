dca98bdc5e76f8b77f03713e156d504092998e83
Stoyan <podlobg@gmail.com>
2024-04-07 14:17:13 +0300

    up

--- Exercise DOM Events/03. Locked-Profile/app.js
+++ Exercise DOM Events/03. Locked-Profile/app.js
@@ -1,3 +1,18 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profileElements = document.querySelectorAll('.profile');
+    
+    for (const profileElement of profileElements) {
+        const showButtonElement = profileElement.querySelector('button');
+        const lockRadioElement = profileElement.querySelector('input[type=radio][value=lock]')
+
+        showButtonElement.addEventListener('click', (e) => {
+            if (lockRadioElement.checked) {
+                return;
+            }
+            
+            const additionInformationElement = showButtonElement.previousElementSibling;
+            additionInformationElement.style.display = 'block'
+        })
+    }
+
 }
