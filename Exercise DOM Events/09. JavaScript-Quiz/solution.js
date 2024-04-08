300fd77e3c92ad5d1f62811ce030753476d404e5
Stoyan <podlobg@gmail.com>
2024-04-08 20:29:36 +0300

    up

--- Exercise DOM Events/09. JavaScript-Quiz/solution.js
+++ Exercise DOM Events/09. JavaScript-Quiz/solution.js
@@ -1,5 +1,5 @@
 function solve() {
-    const quizAnswerElements = document.querySelectorAll('.quiz-asnwer');
+    const quizAnswerElements = document.querySelectorAll('.quiz-answer');
 
     const questions = {
         'Question #1: Which event occurs when the user clicks on an HTML element?': 'onclick',
@@ -7,16 +7,32 @@ function solve() {
         'Question #3: What is DOM?': 'A programming API for HTML and XML documents',
     }
 
-    let asnwerQuestions = 0;
+    let answeredQuestions = 0;
+    for (const answerElement of quizAnswerElements) {
+        answerElement.addEventListener('click', (e) => {
+            const sectionElement = e.currentTarget.parentElement.parentElement;
+            const questionTitle = sectionElement.querySelector('.question h2').textContent;
+            const answerText = answerElement.querySelector('.answer-text').textContent;
 
-    for (const asnwerElement of quizAnswerElements) {
-           asnwerElement.addEventListener('click', (e) => {
-           const questionTitle = e.currentTarget.parentElement.querySelector('.question h2').textContent;
-           const asnwerText = asnwerElement.querySelector('.asnwer-text').textContent;
+            if (questions[questionTitle] == answerText) {
+                answeredQuestions++;
+            }
 
-           if (questions[questionTitle] == asnwerText) {
-              asnwerQuestions++;
-           }
+            const nextSection = sectionElement.nextElementSibling ;
+            sectionElement.classList.add('hidden');
+            sectionElement.style.display = 'none';
+            nextSection.classList.remove('hidden');
+            nextSection.style.display = 'block';
+
+            if (nextSection.id === 'results') {
+                const headingElement = nextSection.querySelector('.results-inner h1');
+                
+                if (answeredQuestions === 3) {
+                    headingElement.textContent = 'You are recognized as top JavaScript fan!'
+                } else {
+                    headingElement.textContent = `You have ${answeredQuestions} right answers`
+                }
+            }
         });
-    };
+    }
 }
