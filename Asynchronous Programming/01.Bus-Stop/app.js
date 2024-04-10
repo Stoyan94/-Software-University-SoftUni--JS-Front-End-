b64402835ec511a976d0af17430ecf273b002ec7
Stoyan <podlobg@gmail.com>
2024-04-10 15:08:28 +0300

    up

--- Asynchronous Programming/01.Bus-Stop/app.js
+++ Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,19 @@
 function getInfo() {
-    // TODO:
-}
+   const baseUrl = 'http://localhost:3030/jsonstore/bus/businfo';
+   const stopIdInputElement = document.getElementById('stopId');
+   const stopNameElement = document.getElementById('stopName');
+   const bussElement = document.getElementById('buses');
+
+    const stopId = stopIdInputElement.value;
+    fetch(`${baseUrl}/${stopId}`)
+        .then(response => response.json())
+        .then(data =>{ stopNameElement.textContent = data.name;
+
+            for (const busId in data.buses) {
+                const liElement = document.createElement('li');
+                liElement.textContent = `Bus ${busId} arrives in ${data.buses[busId]} minutes`;
+                bussElement.appendChild(liElement);
+            }
+        })
+        .catch(() => stopNameElement.textContent ='Error');
+};
--- Asynchronous Programming/Fetch-demo/script.js
+++ Asynchronous Programming/Fetch-demo/script.js
@@ -4,3 +4,8 @@
      .then(response => response.json())
      .then(data => console.log(data))
      .catch(err => console.log('Something went wrong'));
+
+ fetch('localhost:3000/jsonstore/books')
+     .then(response => response.json())
+     .then(data => console.log(data))
+     .catch(error => console.log(error))
